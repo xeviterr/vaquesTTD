95c863582ef76c03fb3993bdabe3638223fad26c
Xevi <xeviterr@gmail.com>
2020-06-29 08:37:21 +0200

    test pes del camio

--- camio.py
+++ camio.py
@@ -2,15 +2,18 @@ from vaca import Vaca
 
 class Camio:
 
-    def __init__(self):
+    def __init__(self, pes=1000):
         super().__init__()
         self.vaques = []
+        self.totalPes = 0
 
     def entra(self, vaca:Vaca):
         self.vaques.append(vaca)
+        self.totalPes += vaca.pes
 
     def treu(self, vaca:Vaca):
         self.vaques.remove(vaca)
+        self.totalPes -= vaca.pes
 
     def __get_nvaques(self):
         return self.vaques.__len__()
--- testVaques.py
+++ testVaques.py
@@ -6,7 +6,7 @@ from raca import Raca
 class TestVaques(unittest.TestCase):
 
     def test_entraTreuVaques(self):
-        c=Camio()
+        c=Camio(300)
         v = Vaca("bonfiassia", 200, Raca("Normanda", 1))
         c.entra(v)
         c.treu(v)
@@ -14,12 +14,17 @@ class TestVaques(unittest.TestCase):
         c.entra(v)
         self.assertTrue(c.nVaques==1)
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_noEsPermetenMesVaquesSiSuperaPes(self):
+        c=Camio(200)
+        v = Vaca("Bonfiassia", 100, Raca("Normanda", 1))
+        c.entra(v)
+        v = Vaca("Maria", 100, Raca("Normanda", 1))
+        c.entra(v)
+        v = Vaca("Joana", 1, Raca("Normanda", 1))
+        c.entra(v)
+        self.assertTrue(c.nVaques == 2, "Error incorrecte en pes maxim camió")
+        
+
 
 if __name__ == '__main__':
     unittest.main()
